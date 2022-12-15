32dab84b8e8415359c5a5b1dbdbad19392d7a901
dianasalasv9 <110954263+dianasalasv9@users.noreply.github.com>
2022-12-14 19:45:05 -0600

    proyecto_js

--- proyecto_js-master/clases/client.js
+++
@@ -1,6 +0,0 @@
-function Client(name, password, total) {
-    this.nameFinal = name;
-    this.passwordFinal = password;
-    this.montoCompra=total;
-  }
-  
--- proyecto_js-master/clases/products.js
+++
@@ -1,8 +0,0 @@
-function Product(id, title, price, images) {
-    this.id = id;
-    this.title = title;
-    this.price= price;
-    this.images = images;
-  }
-  
-  
